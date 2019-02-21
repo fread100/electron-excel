21ce17a2ef56a9aa7ca13aee03d20ad61a467f43
fread100 <fread100@naver.com>
2019-02-21 14:44:35 +0900

    init commit

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 // Modules to control application life and create native browser window
+const path = require('path')
+const glob = require('glob')
 const {app, BrowserWindow} = require('electron')
+
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
@@ -14,6 +17,7 @@ function createWindow () {
       nodeIntegration: true
     }
   })
+  loadInitJs();
 
   // and load the index.html of the app.
   mainWindow.loadFile('index.html')
@@ -54,3 +58,8 @@ app.on('activate', function () {
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
+// Require each JS file in the main-process dir
+function loadInitJs () {
+    const files = glob.sync(path.join(__dirname, 'main-process/**/*.js'))
+    files.forEach((file) => { require(file) })
+}
